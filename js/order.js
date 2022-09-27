d198126b67f0b1db2e100a749c332022b9f8251c
sinyoung98 <joyhope3@naver.com>
2022-09-28 08:35:47 +0900

    edit order.js file
    - promise 버전 연습

--- js/order.js
+++ js/order.js
@@ -5,18 +5,31 @@ staffArea.textContent = "HI!";
 customerArea.textContent ="wow!";
 
 /*callback version */
-const chatbot = () => {
-    /*
-    1. 어서오세요. 서브웨이입니다~! / 2초 
-    2. 메뉴랑 빵 선택해주세요 2초 
-    */
+// const chatbot = () => {
+//     /*
+//     1. 어서오세요. 서브웨이입니다~! / 2초 
+//     2. 메뉴랑 빵 선택해주세요 2초 
+//     */
 
-   setTimeout(() => {
-    staffArea.textContent = "어서오세요. 서브웨이입니다.";
-    setTimeout(() => {
-        staffArea.textContent = "메뉴랑 빵을 선택해주세요";
-    }, 2000);
-   }, 2000)
+//    setTimeout(() => {
+//     staffArea.textContent = "어서오세요. 서브웨이입니다.";
+//     setTimeout(() => {
+//         staffArea.textContent = "메뉴랑 빵을 선택해주세요";
+//     }, 2000);
+//    }, 2000)
+// }
+
+// chatbot();
+
+/*promise version */
+const chatbot = (time, work) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(work());
+        }, time);
+    });
 }
 
-chatbot();
+chatbot(2000, () => staffArea.textContent = "어서오세요. 서브웨이입니다.")
+
+.then(() => chatbot(2000, () => staffArea.textContent = "메뉴랑 빵을 선택해주세요"));
