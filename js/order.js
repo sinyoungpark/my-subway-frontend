bd9b11bf03d36ff631b418bcac4c43ab7bc90891
sinyoung98 <joyhope3@naver.com>
2022-09-28 08:29:17 +0900

    Add order.js file
    - 주문연습 프로그램
    - callback 버전 연습

---
+++ js/order.js
@@ -0,0 +1,22 @@
+const staffArea = document.querySelector(".staff p");
+const customerArea = document.querySelector(".customer p");
+
+staffArea.textContent = "HI!";
+customerArea.textContent ="wow!";
+
+/*callback version */
+const chatbot = () => {
+    /*
+    1. 어서오세요. 서브웨이입니다~! / 2초 
+    2. 메뉴랑 빵 선택해주세요 2초 
+    */
+
+   setTimeout(() => {
+    staffArea.textContent = "어서오세요. 서브웨이입니다.";
+    setTimeout(() => {
+        staffArea.textContent = "메뉴랑 빵을 선택해주세요";
+    }, 2000);
+   }, 2000)
+}
+
+chatbot();
