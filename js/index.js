c320134bf16b73d1208c1a7aecb77e604786776d
sinyoung98 <joyhope3@naver.com>
2022-10-07 23:25:32 +0900

    edit index.js
    - SPA 구현 : list 클릭 시 list.dataset.id를 가지고 있는 section에 active 클래스 추가
    - active : display : block

--- js/index.js
+++ js/index.js
@@ -1,11 +1,36 @@
-const $nav = document.querySelectorAll("#gnb > li");
+/*accordion menu*/
+const makeAccordion = () => {
+  const $nav = document.querySelectorAll("#gnb > li");
 
-$nav.forEach((list) => {
-  list.addEventListener("click", function(){
-    this.classList.toggle("active");
-    const panel = this.children[0];
-    if(panel.classList.contains("gnb-sub-wrap")){
-      panel.style.maxHeight ? panel.style.maxHeight = null : panel.style.maxHeight = panel.scrollHeight + "px";
+  $nav.forEach((list) => {
+    list.addEventListener("click", function(){
+      this.classList.toggle("active");
+      const panel = this.children[0];
+      if(panel.classList.contains("gnb-sub-wrap")){
+        panel.style.maxHeight ? panel.style.maxHeight = null : panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  });  
+}
+
+makeAccordion();
+
+/*SPA */
+const makeSPA = () => {
+  const $list = document.querySelectorAll("#gnb li");
+
+  $list.forEach((list) => {
+    const target = list.dataset.id;
+    if(target){
+      list.addEventListener("click", () => {
+        $section = document.getElementById(target);
+
+        document.querySelector("section.active").classList.remove("active");
+        console.log($section);
+        $section.classList.add("active");
+      });
     }
   });
-});
+}
+
+makeSPA();
