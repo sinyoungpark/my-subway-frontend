7eb08d9563b8b1332e35014293fc9d53737ddcaa
sinyoung98 <joyhope3@naver.com>
2022-11-01 18:27:07 +0900

    edit globalVal.js
    - hideRegisterPage 함수 수정
    header, maintop -> active 클래스 추가
    sect01 -> active 클래스 추가

--- js/globalVar.js
+++ js/globalVar.js
@@ -7,6 +7,8 @@ axios.defaults.withCredentials = true;
 */
 
 /*sections */
+const $header = document.querySelector('header');
+const $mainTop = document.querySelector('.main-top');
 const $sectSignup = document.getElementById("signup");
 const $sectLogin = document.getElementById("login");
 const $sect01 = document.getElementById("sect01");
@@ -17,6 +19,9 @@ const hideRegisterPage = () => {
   if(window.sessionStorage.getItem("accesstoken")){
     $sectSignup.classList.remove("acitve");
     $sectLogin.classList.remove("active");
+    $header.classList.add("active");
+    $mainTop.classList.add("active");
+    $sect01.classList.add("active");
   } else {
     $sectSignup.classList.add("active");
   }
