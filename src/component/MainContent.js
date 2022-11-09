2a4ef68e2a45080b9fcad1e60b6c6d677f0c3de9
sinyoung98 <joyhope3@naver.com>
2022-11-09 20:19:21 +0900

    edit MainContent component
    - createBrowserRouter 로 라우터 설정
    - RouterProvider

--- src/component/MainContent.js
+++ src/component/MainContent.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Maintop from "./Maintop";
 import "../css/MainContent.css";
 import Signup from "./Signup";
 import Login from "./Login";
+import Home from "./Home";
+import {
+  createBrowserRouter, RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element : <Home/>,
+  },
+  {
+    path : "/signup",
+    element : <Signup/>,
+  },
+  {
+    path : "/login",
+    element : <Login/>,
+  },
+])
 
 const MainContent = () => {
+
+
   return(
     <main>
-      {/* <Maintop/> */}
-      {/* <Signup/> */}
-      <Login/>
+      <RouterProvider router={router}></RouterProvider>
     </main>
   )
 }
