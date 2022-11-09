76ea4d5982a0a5aacbe245e7887101e17748cfe7
sinyoung98 <joyhope3@naver.com>
2022-11-09 22:07:39 +0900

    edit Signup component
    - class -> className 수정

--- src/component/Signup.js
+++ src/component/Signup.js
@@ -1,59 +1,73 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "../css/Signup.css";
 import axios from "axios";
+import { Link, Navigate } from "react-router-dom";
 
 const Signup = () => {
   const baseUrl = "http://localhost:8000";
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPW] = useState("");
+  const [success, isSuccess] = useState(false);
 
   const submitSignup = (e) => {
     e.preventDefault();
 
-    if(name && email && password){
-      axios.post(`${baseUrl}/customers/signup`,{
-        name,
-        email,
-        password
-      })
-      .then((res) => res.data)
-      .then((data) => {
-        if(data.error) alert(data.error)
-        else{
-          alert(data.result);
-        }
-      })
-      .catch((error) => alert(error));
-    } else{
-      alert("이름, 이메일, 비밀번호를 입력해주세요.")
+    if (name && email && password) {
+      axios
+        .post(`${baseUrl}/customers/signup`, {
+          name,
+          email,
+          password,
+        })
+        .then((res) => res.data)
+        .then((data) => {
+          if (data.error) alert(data.error);
+          else {
+            alert(data.result);
+            isSuccess(true);
+          }
+        })
+        .catch((error) => alert(error));
+    } else {
+      alert("이름, 이메일, 비밀번호를 입력해주세요.");
     }
-  }
-  
+  };
 
   return (
     <section id="signup" className="section">
+      {success && <Navigate to="/login" replace={true} />}
       <h1 className="logo">
         <span>MY</span> <span>SUB</span>
         <span>WAY</span>
       </h1>
 
       <div className="contents">
-        <section class="left">
-          <p class="main-txt">Already Signed up?</p>
-          <p class="sub-txt">Log in to your account</p>
-          <input type="button" value="LOG IN" class="login_btn" />
+        <section className="left">
+          <p className="main-txt">Already Signed up?</p>
+          <p className="sub-txt">Log in to your account</p>
+          <Link to="/login" className="login_btn">
+            LOG IN
+          </Link>
         </section>
 
-        <section class="right">
-          <p class="main-txt">Sign Up for an Account</p>
-          <p class="sub-txt">
+        <section className="right">
+          <p className="main-txt">Sign Up for an Account</p>
+          <p className="sub-txt">
             Let's go you all set up so you can start creating your first
             onboarding experience.
           </p>
           <form>
             <label for="name">Name</label>
-            <input type="text" name="name" id="name" placeholder="Your name" onChange={(e) => {setName(e.target.value)}}/>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Your name"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
 
             <label for="email">Email</label>
             <input
@@ -61,7 +75,9 @@ const Signup = () => {
               name="email"
               id="email"
               placeholder="Your email"
-              onChange={(e) => {setEmail(e.target.value)}}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
             />
 
             <label for="password">Password</label>
@@ -70,12 +86,14 @@ const Signup = () => {
               name="password"
               id="password"
               placeholder="Your password"
-              onChange={(e) => {setPW(e.target.value)}}
+              onChange={(e) => {
+                setPW(e.target.value);
+              }}
             />
 
-            <input type="submit" value="SIGN UP" onClick={submitSignup}/>
+            <input type="submit" value="SIGN UP" onClick={submitSignup} />
           </form>
-          <div class="social-login">
+          <div className="social-login">
             <p>Or sign up using</p>
             <ul>
               <li>google</li>
