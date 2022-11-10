8957b728cc20354bde6191acffe6f7a59583193a
sinyoung98 <joyhope3@naver.com>
2022-11-10 15:26:17 +0900

    add Logout btn
    - 로그아웃 버튼 추가

--- src/component/Maintop.js
+++ src/component/Maintop.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import SearchIcon from '@mui/icons-material/Search';
+import "../css/Maintop.css";
+import axios from "axios";
 
 const Maintop = () => {
+  const baseUrl = "http://localhost:8000";
+  const [isLogged, setIsLogged] = useState(false);
+  //logout 
+
+  useEffect(() => {
+    if(window.sessionStorage.getItem("accesstoken")){
+      setIsLogged(true);
+    }
+  },[]);
+
+  const logOutHandler = (e) => {
+    e.preventDefault();
+    axios.post(`${baseUrl}/customers/signup`)
+    .then((res) => res.data)
+    .then((data) => {
+      if(data.error) alert(data.error);
+      else{
+        alert(data.result);
+        window.sessionStorage.removeItem("accesstoken");
+      }
+    })
+    .catch((error) => alert(error));
+  }
+
   return (
     <div className="main-top inner">
-      <form action="" class="src-form">
-        <label for="src-value">🔍</label>
+      <form action="" className="src-form">
+        <label for="src-value">
+          <SearchIcon className="search-icon"/>
+        </label>
         <input
           type="text"
           name="src-value"
@@ -12,6 +41,7 @@ const Maintop = () => {
           placeholder="search..."
         />
       </form>
+      <input type="button" value="로그아웃" className="logout-btn" onClick={logOutHandler}/>
     </div>
   );
 };
