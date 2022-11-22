70c97adcda89846e2ceeb14b9915a0f7608dbfb7
sinyoung98 <joyhope3@naver.com>
2022-11-23 03:52:57 +0900

    edit Header styles
    - 반응형 디자인 추가
    - 디자인 수정

--- src/components/Customers/Header/index.js
+++ src/components/Customers/Header/index.js
@@ -1,30 +1,71 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../../../App";
 import profileImg from "../../../img/user-profile.jpg";
 import Navigation from "./Navigation";
-import {HeaderContainer, Profile} from "./styles";
-import { Logo } from "../styles";
+import {
+  DesktopHeader,
+  Profile,
+  ProfileImg,
+  Hamburger,
+  HeaderContainer,
+  MobileHeader,
+} from "./styles";
+import { Logo, LogoutBtn } from "../styles";
+import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
 
-const Header = () => {
+const Header = ({ logoutHandler }) => {
   const [user] = useContext(UserContext);
+  const [activeHamburger, setActiveHamburger] = useState(false);
 
   return (
     <HeaderContainer>
-      <Logo>
-        <Link to="/">
-          <span>MY</span> <span>SUB</span>
-          <span>WAY</span>
-        </Link>
-      </Logo>
+      <DesktopHeader className="only-desktop">
+        <Logo>
+          <Link to="/">
+            <span>MY</span> <span>SUB</span>
+            <span>WAY</span>
+          </Link>
+        </Logo>
 
-      <Profile>
-        <img src={profileImg} alt="user-profile" />
-        <p className="username">{user.name}</p>
-        <p className="email">{user.email}</p>
-      </Profile>
+        <Profile>
+          <ProfileImg>
+            <img src={profileImg} alt="user-profile" />
+          </ProfileImg>
+          <p className="username">{user.name}</p>
+          <p className="email">{user.email}</p>
+        </Profile>
 
-      <Navigation />
+        <Navigation />
+        <LogoutBtn onClick={(e) => logoutHandler(e)}>
+          <LogoutRoundedIcon className="logout-btn" />
+          로그아웃
+        </LogoutBtn>
+      </DesktopHeader>
+
+      <MobileHeader className="only-mobile">
+        <Hamburger
+          className={activeHamburger ? "open" : false}
+          onClick={(e) => setActiveHamburger(!activeHamburger)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </Hamburger>
+        <Logo>
+          <Link to="/">
+            <span>MY</span> <span>SUB</span>
+            <span>WAY</span>
+          </Link>
+        </Logo>
+        <Navigation
+          activeHamburger={activeHamburger}
+          setActiveHamburger={setActiveHamburger}
+        />
+        <LogoutBtn onClick={(e) => logoutHandler(e)}>
+          <LogoutRoundedIcon className="logout-btn" />
+        </LogoutBtn>
+      </MobileHeader>
     </HeaderContainer>
   );
 };
--- src/components/Customers/Header/styles.js
+++ src/components/Customers/Header/styles.js
@@ -1,6 +1,20 @@
 import styled from "styled-components";
 
-export const HeaderContainer = styled.header`
+export const HeaderContainer = styled.div`
+  .only-mobile {
+    display: none;
+  }
+  @media screen and (max-width: 600px) {
+    .only-desktop {
+      display: none;
+    }
+    .only-mobile {
+      display: flex;
+    }
+  }
+`;
+
+export const DesktopHeader = styled.header`
   grid-area: a;
   height: 100vh;
   font-family: var(--font-primary);
@@ -8,19 +22,77 @@ export const HeaderContainer = styled.header`
   position: fixed;
   top: 0;
   background-color: #fff;
-  padding: 0 60px;
+  padding: 0 50px;
   z-index: 99;
+`;
+
+export const MobileHeader = styled.header`
+  width: 100%;
+  height: 70px;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 99;
+  position : fixed;
+`;
+
+export const Hamburger = styled.div`
+  position: absolute;
+  cursor: pointer;
+  left: 20px;
+  display: flex;
+  width: 32px;
+  height: 30px;
+  flex-direction: column;
+  justify-content: space-between;
+  span {
+    display: inline-block;
+    width: 100%;
+    height: 8px;
+    border-radius: 10px;
+    background-color: var(--color-green-1);
+    transition: transform 0.3s;
+  }
+
+  &.open {
+    span:nth-child(1) {
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%) rotate(45deg);
+    }
+    span:nth-child(2) {
+      display: none;
+    }
+    span:nth-child(3) {
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%) rotate(-45deg);
+    }
+  }
+`;
+
+export const MobileNav = styled.nav``;
+
+export const ProfileImg = styled.div`
+  border: 1px solid var(--color-green-1);
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  padding: 2px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+  }
 `;
 
 export const Profile = styled.div`
   flex-direction: column;
   text-align: center;
-
-  img {
-    width: 6rem;
-    height: 6rem;
-    border-radius: 50%;
-  }
+  display: flex;
+  align-items: center;
 
   p.username {
     font-size: 24px;
@@ -33,9 +105,11 @@ export const Profile = styled.div`
   }
 `;
 
+export const Gnb = styled.ul``;
+
 export const Nav = styled.nav`
   margin: 5rem 0;
-  font-size: 18px;
+  font-size: 20px;
 
   #gnb {
     > li {
@@ -80,6 +154,31 @@ export const Nav = styled.nav`
     .active {
       .gnb-sub-wrap {
         border-top: 1px solid var(--color-grey-1);
+      }
+    }
+  }
+
+  @media screen and (max-width: 600px) {
+    background-color: #fff;
+    margin: 0;
+    position: absolute;
+    top: 70px;
+    width: 100%;
+    z-index: 99;
+    height: 100vh;
+    display: none;
+    &.open {
+      display: block;
+    }
+
+    #gnb {
+      padding: 20px 30px;
+      font-size: 24px;
+      > li {
+        margin-top: 50px;
+        .gnb-sub-wrap {
+          font-size: 18px;
+        }
       }
     }
   }
