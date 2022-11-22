45acbce70753578f8d8a1aeb28cc7c2ffa6e3c07
sinyoung98 <joyhope3@naver.com>
2022-11-23 03:54:09 +0900

    edit Home component
    - 반응형 디자인 추가
    - 스타일 수정

--- src/components/Customers/Home/SlideCard.js
+++ src/components/Customers/Home/SlideCard.js
@@ -4,9 +4,6 @@ import { Slide } from "./styles";
 const SlideCard = React.forwardRef(({ adData }, ref) => {
   return (
     <Slide ref={ref}>
-      {
-        console.log(adData)
-      }
       <img src={adData.img} alt="" className="ad-img" />
     </Slide>
   );
